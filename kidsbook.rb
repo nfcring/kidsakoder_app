5bcb91b7fd7bec87dc82f163ec425962e165d147
Asbjørn Thorsen <asbjornthorsen@Asbjrns-iMac.local>
2016-11-25 15:40:06 +0100

    hei jeg er en bråshan

--- kidsbook.rb
+++ kidsbook.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+
 set(:bind, "0.0.0.0")
+
 get '/' do
   erb :index
+end
+
+post '/behandle-login' do
+  p params            # skriv ut skjema data
+  redirect to("/")    # omdiriger til startsiden
 end
